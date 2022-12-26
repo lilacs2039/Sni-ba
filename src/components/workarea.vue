0f1a2b95808e197c7f70d32557ff779cb8740ec3
lilacs2039 <quantum.methods@gmail.com>
2022-12-26 21:44:34 +0900

    投稿ボタン　切替式に  view, post, new lang…

--- src/components/workarea.vue
+++ src/components/workarea.vue
@@ -3,23 +3,51 @@ import { ref, reactive, inject } from "vue";
 import view_area from "/src/components/view-area.vue";
 import post_area from "/src/components/post-area.vue";
 import iconButton from "/src/components/iconButton.vue";
+import iconTextButton from "./iconTextButton.vue";
+import { langStrKey } from "../store/keys";
 // import { EditorContext } from "../store/EditorContext";
 // import { editorContextKey } from "../store/keys";
 // import { decode } from "rison";
 // const editorContext = inject(editorContextKey) as EditorContext;
 
-var showViewArea = ref(true);
+const langStr = inject(langStrKey);
+const user = "lilacs2039";
 
-function switchShow() {
-  showViewArea.value = !showViewArea.value;
-  console.log(showViewArea)
+var showViewArea = ref(true);
+function showView() {
+  showViewArea.value = true;
+}
+function showPost() {
+  showViewArea.value = false;
+}
+function newLang() {
+  // GitHubのページへジャンプ
+  window.open(
+    `https://github.com/${user}/Sni-ba-snippets/new/main`,
+    "_blank",
+    "noreferrer"
+  );
 }
 </script>
 
 <template>
   <div class="workarea">
     <div class="menu">
-      <iconButton caption="投稿" icon="/img/upload.png" @click="switchShow" />
+      <icon-text-button
+        text="View"
+        icon="/img/side-right-view.png"
+        @click="showView"
+      />
+      <icon-text-button
+        text="Post..."
+        icon="/img/upload.png"
+        @click="showPost"
+      />
+      <icon-text-button
+        text="New lang..."
+        icon="/img/blank-file.png"
+        @click="newLang"
+      />
     </div>
     <view_area v-show="showViewArea" />
     <post_area v-show="!showViewArea" />
@@ -27,12 +55,10 @@ function switchShow() {
 </template>
 
 <style>
-
 .menu {
   display: flex;
+  gap: 10px;
 }
-
-
 </style>
 
 
